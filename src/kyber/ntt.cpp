3dc22e87dc3af7f0222fdd5b84ddddbf19b5de84
mikeyang <18604713262@163.com>
2024-04-13 20:16:43 -0400

    .gitignore

---
+++ src/kyber/ntt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Yang on 2024/4/13.
+//
+
+#include "ntt.h"
---
+++ src/kyber/ntt.h
@@ -0,0 +1,14 @@
+//
+// Created by Mike Yang on 2024/4/13.
+//
+
+#ifndef CC20_NTT_H
+#define CC20_NTT_H
+
+
+class ntt {
+
+};
+
+
+#endif //CC20_NTT_H
