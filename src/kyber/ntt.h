2749cf2950ac88df74d8b6f9f6ac28a30c80836e
mikeyang <18604713262@163.com>
2024-05-03 15:33:52 -0400

    Assembly implementation for timings.

--- src/kyber/ntt.h
+++ src/kyber/ntt.h
@@ -1,14 +1,16 @@
 //
-// Created by Mike Yang on 2024/4/13.
+// Created by Yi Yang on 2024/4/13.
 //
 
 #ifndef CC20_NTT_H
 #define CC20_NTT_H
 
 
+#include <vector>
+
 class ntt {
 public:
-
+  void parse (const std::vector<uint8_t>& v, std::vector<uint8_t> & out) ;
 private:
   int q = 3329;
 };
