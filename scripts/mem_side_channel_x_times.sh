d3c5d812d9ee7df8d4caf30c1fa1658c23f1ce7b
mikeyang <18604713262@163.com>
2024-05-04 20:27:36 -0400

    Test running script.

--- scripts/mem_side_channel_x_times.sh
+++ scripts/mem_side_channel_x_times.sh
@@ -5,7 +5,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-command="printf \"1234\n\" | ./cc20 -E debian.iso -H -j"
+command="printf \"1234\n\" | ./cc20 -E debian.iso -H -d -j"
 num_iterations=$1
 
 for ((i=1; i<=num_iterations; i++)); do
