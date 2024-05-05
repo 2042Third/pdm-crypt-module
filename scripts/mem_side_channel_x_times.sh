7e5f7faf9740f3eeb95b77ea937f392e1ea0408e
mikeyang <18604713262@163.com>
2024-05-04 20:54:08 -0400

    TRest script runner.

--- scripts/mem_side_channel_x_times.sh
+++ scripts/mem_side_channel_x_times.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <num_iterations>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <num_iterations> <options>"
     exit 1
 fi
 
-command="printf \"1234\n\" | ./cc20 -E debian.iso -H -d -c 5 -j"
+command="printf \"1234\n\" | ./cc20 -E debian.iso -j"
+options=$2
 num_iterations=$1
 
 for ((i=1; i<=num_iterations; i++)); do
-    eval "$command $i"
+    eval "$command $i $options"
     echo "Iteration $i completed."
 done
