4cb58939bb9ecbf8b67e3d73abb261f888c6a45b
mikeyang <18604713262@163.com>
2024-05-04 20:04:20 -0400

    Some script for running the test faster.

---
+++ scripts/mem_side_channel_x_times.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <command> <num_iterations>"
+    exit 1
+fi
+
+command="printf \"1234\n\" | ./cc20 -E debian.iso -H -j "
+num_iterations=$2
+
+for ((i=1; i<=$num_iterations; i++)); do
+    $command $i
+    echo "Iteration $i completed."
+done
